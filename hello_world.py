c7c7a186dd301a4acd91d3f078845c1abdb57638
Ally Cho <allyjcho@gmail.com>
2020-04-13 17:25:08 -0500

    assignment: hello world

---
+++ hello_world.py
@@ -0,0 +1,36 @@
+print("Hello World!")
+
+name = "Ally"
+print("Hello", name)
+
+name = "Ally"
+print("Hello " + name)
+
+name = 42
+print("Hello", name)
+
+name = 42
+print("Hello" + name)
+
+name = 42
+print("Hello %d" % name)
+
+fave_food1 = "ramen"
+fave_food2 = "lobster"
+print("I love to eat {} and {}.".format(fave_food1, fave_food2))
+
+fave_food1 = "ramen"
+fave_food2 = "lobster"
+print("I love to eat {fave_food1} and {fave_food2}.")
+
+x = "hello world"
+print(x.title())
+
+x = "hello world"
+print(x.upper())
+
+x = "Hello World"
+print(x.lower())
+
+x = "hello world"
+print(x.split())
